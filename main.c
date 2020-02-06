95d73d0de0b1d8331192b6e71033e8a769e6695c
farisNassif <g00347032@gmit.ie>
2020-02-06 13:48:49 +0000

    File reading implemented

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Testonetwothree");
+int main(int argc, char *argv[]){
+    FILE *fptr; 
+  
+    char filename[100], c; 
+  
+    // printf("Enter the filename to open \n"); 
+    // scanf("%s", filename); 
+  
+    // Open file 
+    fptr = fopen("Input\\FileReadTest.txt", "r"); 
+    if (fptr == NULL) { 
+        printf("Cannot open file \n"); 
+        exit(0); 
+    } 
+  
+    // Read contents from file 
+    c = fgetc(fptr); // Gets next char from parameterized stream 
+    while (c != EOF) { 
+        printf ("%c", c); 
+        c = fgetc(fptr); 
+    } 
+  
+    fclose(fptr); 
+    return 0; 
 }
