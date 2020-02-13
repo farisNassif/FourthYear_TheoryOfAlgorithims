e098025fd33c22319440bb0ab89b504565191d91
farisNassif <g00347032@gmit.ie>
2020-02-13 14:04:45 +0000

    Changes made to file input in main(), added some lovely comments and func docs

--- Program/main.c
+++ Program/main.c
@@ -1,28 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+ * Author: 		Faris Nassif
+ * Module: 	    Theory Of Algorithms
+ * Summary:     A program that executes the Secure Hash Algorithim(SHA-256) on an input 
+*/
 
-int main(int argc, char *argv[]){
+#include <stdio.h> // Input/Output
+#include <stdlib.h> // TODO
+#include <string.h>
+
+char filename[100], c; // Temp for storing the name of file read
+
+/*
+ * Acts as the Runner of the program.
+ * 
+ * @Paramaters
+ * ------------------------------------
+ * *argc: The num of paramaters that are used
+ * *argv[]: Array of paramater names
+ * 
+ * @Returns
+ * ------------------------------------
+ * TODO ? Returns 0
+*/
+int main(int argc, char *argv[]) {
+    // Pointer for file
     FILE *fptr; 
   
-    char filename[100], c; 
-  
-    // printf("Enter the filename to open \n"); 
-    // scanf("%s", filename); 
-  
-    // Open file 
-    fptr = fopen("Input\\FileReadTest.txt", "r"); 
-    if (fptr == NULL) { 
-        printf("Cannot open file \n"); 
-        exit(0); 
-    } 
-  
-    // Read contents from file 
-    c = fgetc(fptr); // Gets next char from parameterized stream 
-    while (c != EOF) { 
-        printf ("%c", c); 
-        c = fgetc(fptr); 
-    } 
-  
-    fclose(fptr); 
+  	// Only runs if an argument was passed as a param
+	if (argv[1] == NULL)
+	{
+		printf("no file specified as an input\n");
+	} else {
+        fptr = fopen(argv[1], "rb");
+        strcpy(filename, argv[1]);
+
+        // If there was no file or an issue with opening it
+	    if (fptr == NULL) { 
+		    printf("Can't open it :(\n");
+	    } else {
+		    printf("It worked yay\n");
+		    printf("Filename: %s\n", filename);
+            // Do SHA ..
+            
+            c = fgetc(fptr); // Gets next char from parameterized stream 
+            while (c != EOF) { 
+                printf ("%c", c); 
+                c = fgetc(fptr); 
+            } 
+        }
+    }
+
+    fclose(fptr); // Close the file ..
     return 0; 
 }
