8540c767f3ba2bb07558d44b961a76801a115b2f
farisNassif <g00347032@gmit.ie>
2020-02-29 14:04:45 +0000

    Hash function implemented, couple changes and other implementations made to main.c

--- Program/main.c
+++ Program/main.c
@@ -8,6 +8,24 @@
 #include <stdint.h> // Req for uint(x) unsigned int
 
 /*
+    Page 11 - 4.2.2
+    Constants representing the first 32 bits of the fractional parts of
+    the cube roots of mthe first sixty four prime numbers required for the algorithim.
+    
+    The 0x prefix denotes the number succeeding it is being written in hex
+*/
+const uint32_t K[64] = {
+	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+	0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+    0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+    0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+	0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+	0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+};
+
+/*
     [Majority Function]
     =>  Produces a 1 if and ONLY if the majority of inputs are 1's, otherwise output a 0.
     =>  Section 4.1.2
@@ -86,6 +104,41 @@ union block {
     uint32_t threetwo[16];
     uint8_t eight[64];
 };
+
+/*
+    6.6.2 Hash Standard
+    Takes M and H, overrides H
+*/
+
+void nexthash(union block *M, uint32_t *H) {
+    uint32_t W[64];
+    uint32_t a, b, c, d, e, f, g, h, T1, T2; // Should become what current H[] is
+    int t;
+
+    // 6.2.2
+    for (t = 0; t < 16; t++) {
+        W[t] = M->threetwo[t];
+    }
+
+    for (t = 16; t < 16; t++) {
+        W[t] = sig_one(W[t-2]) + W[t-7] + sig_zero(W[t-15]) + W[t-16];
+    }
+
+    a = H[0]; b = H[1]; c = H[2]; d = H[3];
+    e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+    for (t = 0; t < 65; t++) {
+        T1 = h + Sig1(e) + Ch(e, f, g) + K[t] + W[t];
+        T2 = Sig0(a) + Maj(a, b, c);
+        h = g; g = f; f = e; e = d + T1;
+        d = c; c = b; b = a; a = T1 + T2;
+
+        // Compute the i'th intermediate value of H[]
+        H[0] += a; H[1] += b; H[2] += c;
+        H[3] += d; H[4] += e; H[5] += f;
+        H[6] += g; H[7] += f;
+    }
+}
 
 void md5();
 void writeToFile();
