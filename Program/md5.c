8cf27248eadd3316eb04704c6bbd4482508f312c
farisNassif <g00347032@gmit.ie>
2020-03-26 19:04:45 +0000

    Rewatched some videos, decomplicated program and refactored some items

--- Program/md5.c
+++ Program/md5.c
@@ -35,7 +35,6 @@
 
 /* 
     https://tools.ietf.org/html/rfc1321 => Page 10
-
     [FF, GG, HH, II] => Transformations for rounds 1, 2, 3, and 4
     The first 4 Paramaters for each function are the four 16 bit Words
     The fifth Paramater consists of the union block message (Input for MD5 is 64 bytes / 16 x 32 bit)
@@ -108,20 +107,6 @@ typedef union {
     uint8_t eight[64];
 } BLOCK;
 
-/* UINT4 defines a four byte word */
-typedef unsigned long int UINT4;
-
-/*
-    https://tools.ietf.org/html/rfc1321 => Page 9
-
-    MD5 Context defining State(ABCD), Count(Num of bits) and input buffer
-*/
-typedef struct {
-  UINT4 state[4];   
-  UINT4 count[2];     
-  unsigned char buffer[64];  
-} MD5_CONTEXT;
-
 /*
     Status controller when reading the pad file
 
@@ -135,153 +120,134 @@ typedef enum {
     FINISH 
 } PADFLAG;
 
-/* 
-    MD5 initialization. Begins an MD5 operation, writing a new context.
-*/
-void MD5Init (MD5_CONTEXT *context) {
-  context->count[0] = context->count[1] = 0;
-  /* Load magic initialization constants.
-*/
-  context->state[0] = A;
-  context->state[1] = B;
-  context->state[2] = C;
-  context->state[3] = D;
-}  
-
-// Temp for outputting, will change later
-uint32_t output[4];
-
 /* ----------------------- MD5 Implementation ------------------------ */
-void md5(UINT4 *state, BLOCK *M) {
-    WORD a = A, b = B, c = C, d = D;
-    a = state[0];
-    b = state[1];
-    c = state[2];
-    d = state[3];
+void md5(BLOCK *M, uint32_t *H) {
+    WORD a, b, c, d;
+    a = H[0];
+    b = H[1];
+    c = H[2];
+    d = H[3];
 
-    // Round 1
-    // This should be looped once everything starts working
-    FF (a, b, c, d, M->threetwo[0] , S[0] , T[0]) ; /* 1  */
-    FF (d, a, b, c, M->threetwo[1] , S[1] , T[1]) ; /* 2  */
-    FF (c, d, a, b, M->threetwo[2] , S[2] , T[2]) ; /* 3  */
-    FF (b, c, d, a, M->threetwo[3] , S[3] , T[3]) ; /* 4  */
-    FF (a, b, c, d, M->threetwo[4] , S[4] , T[4]) ; /* 5  */
-    FF (d, a, b, c, M->threetwo[5] , S[5] , T[5]) ; /* 6  */
-    FF (c, d, a, b, M->threetwo[6] , S[6] , T[6]) ; /* 7  */
-    FF (b, c, d, a, M->threetwo[7] , S[7] , T[7]) ; /* 8  */
-    FF (a, b, c, d, M->threetwo[8] , S[8] , T[8]) ; /* 9  */
-    FF (d, a, b, c, M->threetwo[9] , S[9] , T[9]) ; /* 10 */
-    FF (c, d, a, b, M->threetwo[10], S[10], T[10]); /* 11 */
-    FF (b, c, d, a, M->threetwo[11], S[11], T[11]); /* 12 */
-    FF (a, b, c, d, M->threetwo[12], S[12], T[12]); /* 13 */
-    FF (d, a, b, c, M->threetwo[13], S[13], T[13]); /* 14 */
-    FF (c, d, a, b, M->threetwo[14], S[14], T[14]); /* 15 */
-    FF (b, c, d, a, M->threetwo[15], S[15], T[15]); /* 16 */
-    
-    // Round 2
-    GG (a, b, c, d, M->threetwo[1] , S[16], T[16]); /* 17 */
-    GG (d, a, b, c, M->threetwo[6] , S[17], T[17]); /* 18 */
-    GG (c, d, a, b, M->threetwo[11], S[18], T[18]); /* 19 */
-    GG (b, c, d, a, M->threetwo[0] , S[19], T[19]); /* 20 */
-    GG (a, b, c, d, M->threetwo[5] , S[20], T[20]); /* 21 */
-    GG (d, a, b, c, M->threetwo[10], S[21], T[21]); /* 22 */
-    GG (c, d, a, b, M->threetwo[15], S[22], T[22]); /* 23 */
-    GG (b, c, d, a, M->threetwo[4] , S[23], T[23]); /* 24 */
-    GG (a, b, c, d, M->threetwo[9] , S[24], T[24]); /* 25 */
-    GG (d, a, b, c, M->threetwo[14], S[25], T[25]); /* 26 */
-    GG (c, d, a, b, M->threetwo[3] , S[26], T[26]); /* 27 */
-    GG (b, c, d, a, M->threetwo[8] , S[27], T[27]); /* 28 */
-    GG (a, b, c, d, M->threetwo[13], S[28], T[28]); /* 29 */
-    GG (d, a, b, c, M->threetwo[2] , S[29], T[29]); /* 30 */
-    GG (c, d, a, b, M->threetwo[7] , S[30], T[30]); /* 31 */
-    GG (b, c, d, a, M->threetwo[12], S[31], T[31]); /* 32 */
+    /* Round 1 */
+    FF(H[0], H[1], H[2], H[3], M->threetwo[0] , S[0] , T[0]) ; /*  1 */
+    FF(H[3], H[0], H[1], H[2], M->threetwo[1] , S[1] , T[1]) ; /*  2 */
+    FF(H[2], H[3], H[0], H[1], M->threetwo[2] , S[2] , T[2]) ; /*  3 */
+    FF(H[1], H[2], H[3], H[0], M->threetwo[3] , S[3] , T[3]) ; /*  4 */
+    FF(H[0], H[1], H[2], H[3], M->threetwo[4] , S[4] , T[4]) ; /*  5 */
+    FF(H[3], H[0], H[1], H[2], M->threetwo[5] , S[5] , T[5]) ; /*  6 */
+    FF(H[2], H[3], H[0], H[1], M->threetwo[6] , S[6] , T[6]) ; /*  7 */
+    FF(H[1], H[2], H[3], H[0], M->threetwo[7] , S[7] , T[7]) ; /*  8 */
+    FF(H[0], H[1], H[2], H[3], M->threetwo[8] , S[8] , T[8]) ; /*  9 */
+    FF(H[3], H[0], H[1], H[2], M->threetwo[9] , S[9] , T[9]) ; /* 10 */
+    FF(H[2], H[3], H[0], H[1], M->threetwo[10], S[10], T[10]); /* 11 */
+    FF(H[1], H[2], H[3], H[0], M->threetwo[11], S[11], T[11]); /* 12 */
+    FF(H[0], H[1], H[2], H[3], M->threetwo[12], S[12], T[12]); /* 13 */
+    FF(H[3], H[0], H[1], H[2], M->threetwo[13], S[13], T[13]); /* 14 */
+    FF(H[2], H[3], H[0], H[1], M->threetwo[14], S[14], T[14]); /* 15 */
+    FF(H[1], H[2], H[3], H[0], M->threetwo[15], S[15], T[15]); /* 16 */
+    printf("\n");
+//    /* Round 2 */
+    GG(H[0], H[1], H[2], H[3], M->threetwo[1] , S[16], T[16]); /* 17 */
+    GG(H[3], H[0], H[1], H[2], M->threetwo[6] , S[17], T[17]); /* 18 */
+    GG(H[2], H[3], H[0], H[1], M->threetwo[11], S[18], T[18]); /* 19 */
+    GG(H[1], H[2], H[3], H[0], M->threetwo[0] , S[19], T[19]); /* 20 */
+    GG(H[0], H[1], H[2], H[3], M->threetwo[5] , S[20], T[20]); /* 21 */
+    GG(H[3], H[0], H[1], H[2], M->threetwo[10], S[21], T[21]); /* 22 */
+    GG(H[2], H[3], H[0], H[1], M->threetwo[15], S[22], T[22]); /* 23 */
+    GG(H[1], H[2], H[3], H[0], M->threetwo[4] , S[23], T[23]); /* 24 */
+    GG(H[0], H[1], H[2], H[3], M->threetwo[9] , S[24], T[24]); /* 25 */
+    GG(H[3], H[0], H[1], H[2], M->threetwo[14], S[25], T[25]); /* 26 */
+    GG(H[2], H[3], H[0], H[1], M->threetwo[3] , S[26], T[26]); /* 27 */
+    GG(H[1], H[2], H[3], H[0], M->threetwo[8] , S[27], T[27]); /* 28 */
+    GG(H[0], H[1], H[2], H[3], M->threetwo[13], S[28], T[28]); /* 29 */
+    GG(H[3], H[0], H[1], H[2], M->threetwo[2] , S[29], T[29]); /* 30 */
+    GG(H[2], H[3], H[0], H[1], M->threetwo[7] , S[30], T[30]); /* 31 */
+    GG(H[1], H[2], H[3], H[0], M->threetwo[12], S[31], T[31]); /* 32 */
+    printf("\n");
+//    /* Round 3 */
+    HH(H[0], H[1], H[2], H[3], M->threetwo[5] , S[32], T[32]); /* 33 */
+    HH(H[3], H[0], H[1], H[2], M->threetwo[8] , S[33], T[33]); /* 34 */
+    HH(H[2], H[3], H[0], H[1], M->threetwo[11], S[34], T[34]); /* 35 */
+    HH(H[1], H[2], H[3], H[0], M->threetwo[14], S[35], T[35]); /* 36 */
+    HH(H[0], H[1], H[2], H[3], M->threetwo[1] , S[36], T[36]); /* 37 */
+    HH(H[3], H[0], H[1], H[2], M->threetwo[4] , S[37], T[37]); /* 38 */
+    HH(H[2], H[3], H[0], H[1], M->threetwo[7] , S[38], T[38]); /* 39 */
+    HH(H[1], H[2], H[3], H[0], M->threetwo[10], S[39], T[39]); /* 40 */
+    HH(H[0], H[1], H[2], H[3], M->threetwo[13], S[40], T[40]); /* 41 */
+    HH(H[3], H[0], H[1], H[2], M->threetwo[0] , S[41], T[41]); /* 42 */
+    HH(H[2], H[3], H[0], H[1], M->threetwo[3] , S[42], T[42]); /* 43 */
+    HH(H[1], H[2], H[3], H[0], M->threetwo[6] , S[43], T[43]); /* 44 */
+    HH(H[0], H[1], H[2], H[3], M->threetwo[9] , S[44], T[44]); /* 45 */
+    HH(H[3], H[0], H[1], H[2], M->threetwo[12], S[45], T[45]); /* 46 */
+    HH(H[2], H[3], H[0], H[1], M->threetwo[15], S[46], T[46]); /* 47 */
+    HH(H[1], H[2], H[3], H[0], M->threetwo[2] , S[47], T[47]); /* 48 */
+    printf("\n");
+//    /* Round 4 */
+    II(H[0], H[1], H[2], H[3], M->threetwo[0] , S[48], T[48]); /* 49 */
+    II(H[3], H[0], H[1], H[2], M->threetwo[7] , S[49], T[49]); /* 50 */
+    II(H[2], H[3], H[0], H[1], M->threetwo[14], S[50], T[50]); /* 51 */
+    II(H[1], H[2], H[3], H[0], M->threetwo[5] , S[51], T[51]); /* 52 */
+    II(H[0], H[1], H[2], H[3], M->threetwo[12], S[52], T[52]); /* 53 */
+    II(H[3], H[0], H[1], H[2], M->threetwo[3] , S[53], T[53]); /* 54 */
+    II(H[2], H[3], H[0], H[1], M->threetwo[10], S[54], T[54]); /* 55 */
+    II(H[1], H[2], H[3], H[0], M->threetwo[1] , S[55], T[55]); /* 56 */
+    II(H[0], H[1], H[2], H[3], M->threetwo[8] , S[56], T[56]); /* 57 */
+    II(H[3], H[0], H[1], H[2], M->threetwo[15], S[57], T[57]); /* 58 */
+    II(H[2], H[3], H[0], H[1], M->threetwo[6] , S[58], T[58]); /* 59 */
+    II(H[1], H[2], H[3], H[0], M->threetwo[13], S[59], T[59]); /* 60 */
+    II(H[0], H[1], H[2], H[3], M->threetwo[4] , S[60], T[60]); /* 61 */
+    II(H[3], H[0], H[1], H[2], M->threetwo[11], S[61], T[61]); /* 62 */
+    II(H[2], H[3], H[0], H[1], M->threetwo[2] , S[62], T[62]); /* 63 */
+    II(H[1], H[2], H[3], H[0], M->threetwo[9] , S[63], T[63]); /* 64 */
+    printf("\n");
 
-    // Round 3
-    HH (a, b, c, d, M->threetwo[5] , S[32], T[32]); /* 33 */
-    HH (d, a, b, c, M->threetwo[8] , S[33], T[33]); /* 34 */
-    HH (c, d, a, b, M->threetwo[11], S[34], T[34]); /* 35 */
-    HH (b, c, d, a, M->threetwo[14], S[35], T[35]); /* 36 */
-    HH (a, b, c, d, M->threetwo[1] , S[36], T[36]); /* 37 */
-    HH (d, a, b, c, M->threetwo[4] , S[37], T[37]); /* 38 */
-    HH (c, d, a, b, M->threetwo[7] , S[38], T[38]); /* 39 */
-    HH (b, c, d, a, M->threetwo[10], S[39], T[39]); /* 40 */
-    HH (a, b, c, d, M->threetwo[13], S[40], T[40]); /* 41 */
-    HH (d, a, b, c, M->threetwo[0] , S[41], T[41]); /* 42 */
-    HH (c, d, a, b, M->threetwo[3] , S[42], T[42]); /* 43 */
-    HH (b, c, d, a, M->threetwo[6] , S[43], T[43]); /* 44 */
-    HH (a, b, c, d, M->threetwo[9] , S[44], T[44]); /* 45 */
-    HH (d, a, b, c, M->threetwo[12], S[45], T[45]); /* 46 */
-    HH (c, d, a, b, M->threetwo[15], S[46], T[46]); /* 47 */
-    HH (b, c, d, a, M->threetwo[2] , S[47], T[47]); /* 48 */
-
-    // Fourth and final round
-    II (a, b, c, d, M->threetwo[0] , S[47], T[48]); /* 49 */
-    II (d, a, b, c, M->threetwo[7] , S[47], T[49]); /* 50 */
-    II (c, d, a, b, M->threetwo[14], S[47], T[50]); /* 51 */
-    II (b, c, d, a, M->threetwo[5] , S[47], T[51]); /* 52 */
-    II (a, b, c, d, M->threetwo[12], S[47], T[52]); /* 53 */
-    II (d, a, b, c, M->threetwo[3] , S[47], T[53]); /* 54 */
-    II (c, d, a, b, M->threetwo[10], S[47], T[54]); /* 55 */
-    II (b, c, d, a, M->threetwo[1] , S[47], T[55]); /* 56 */
-    II (a, b, c, d, M->threetwo[8] , S[47], T[56]); /* 57 */
-    II (d, a, b, c, M->threetwo[15], S[47], T[57]); /* 58 */
-    II (c, d, a, b, M->threetwo[6] , S[47], T[58]); /* 59 */
-    II (b, c, d, a, M->threetwo[13], S[47], T[59]); /* 60 */
-    II (a, b, c, d, M->threetwo[4] , S[47], T[60]); /* 61 */
-    II (d, a, b, c, M->threetwo[11], S[47], T[61]); /* 62 */
-    II (c, d, a, b, M->threetwo[2] , S[47], T[62]); /* 63 */
-    II (b, c, d, a, M->threetwo[9] , S[47], T[63]); /* 64 */
-
-    output[0] += a;
-    output[1] += b;
-    output[2] += c;
-    output[3] += d;
+    H[0] += a;
+    H[1] += b;
+    H[2] += c;
+    H[3] += d;
 }
 
-/* ----------------------------- Padding ----------------------------- */
-int pad(BLOCK *M, FILE *infile, uint64_t *nobits, PADFLAG *status) {
-  int i;
-  size_t nobytesread;
+/* ----------------------- Read Block by Block ----------------------- */
+int nextblock(BLOCK *M, FILE *infile, uint64_t *nobits, PADFLAG *status) {
 
-  /* Before stuff gets read in, need to check the value of status */  
+  int i;
+  size_t nobytesread = fread(&M->eight, 1, 64, infile);
+  *nobits += nobytesread * 8;
+
+    /* Before stuff gets read in, need to check the value of status */  
     switch(*status) {
     /* If finished, return */
     case FINISH:
         return 0;
     case PAD0:
         // We need an all-padding block without the 1 bit.
-        for (int i = 0; i < 56; i++) {
-            M->eight[i] = 0x00;
+        for (i = 0; i < 57; i++) {
+            M->eight[i] = 0;
         }
         M->sixfour[7] = *nobits;
         *status = FINISH;
-        break;
+        return 1;
+        break;  
     default:
-        // Try to read 64 bytes from the file.
-        nobytesread = fread(M->eight, 1, 64, infile);
-        *nobits += (8ULL * ((uint64_t) nobytesread));
         if (nobytesread < 56) {
             // We can put all padding in this block.
             M->eight[nobytesread] = 0x80;
             for (i = nobytesread + 1; i < 56; i++){
-                M->eight[i] = 0x00;
+                M->eight[i] = 0;
             }
             M->sixfour[7] = *nobits;
             *status = FINISH;
-        } else if (nobytesread < 64) {
+        } 
+        else if (nobytesread < 64) {
             // Otherwise we have read between 56 (incl) and 64 (excl) bytes.
             M->eight[nobytesread] = 0x80;
-            for (int i = nobytesread + 1; i < 64; i++) {
-                M->eight[i] = 0x00;
+            for (i = nobytesread + 1; i < 64; i++) {
+                M->eight[i] = 0;
             }
             *status = PAD0;
         }
+        return 1;
     }
-    return 1;
-}
-/* ----------------------- MD5 Pre Processor ------------------------- */
-void MD5_Prepare(MD5_CONTEXT *context, char *da) {
-
 }
 
 /* -------------------------- Main Method ---------------------------- */
@@ -297,26 +263,21 @@ int main(int argc, char *argv[]) {
         printf("Error: couldn't open file %s.\n", argv[1]);
         return 1;
     }
-    
-    MD5_CONTEXT _MD5_CONTEXT;
     // The current padded message block
     BLOCK M;
     uint64_t nobits = 0;
     PADFLAG status = READ;
-    char strToHash = 'abc';
+    uint32_t H[] = {A, B, C, D};
 
     // Read through all of the padded message blocks.
-    while (pad(&M, infile, &nobits, &status)) {
+    while (nextblock(&M, infile, &nobits, &status)) {
         // Calculate the next hash value.
-        MD5_Prepare(&_MD5_CONTEXT, strToHash);
-        //md5(&M);
+        md5(&M, H);
     }
 
     for(int i=0;i<4;i++) {
-        printf("%02x", output[i]);
+        printf("%02x", H[i]);
     }
-
-    printf("\n");
 
     fclose(infile);
 
